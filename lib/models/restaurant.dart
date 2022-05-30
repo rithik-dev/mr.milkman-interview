85a8cdd6356eba55a65c122a01c51a5cadb3f7e0
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-05-30 18:06:36 +0530

    feat: added restaurant model and card

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,83 @@
+class Restaurant {
+  final String id;
+  final String name;
+  final double rating;
+  final List<String> images;
+  final bool isPureVeg;
+  final String subtitle;
+  final String costForOneText;
+  final String deliveryTime;
+  final String discount;
+  final String proDiscount;
+
+// discount?
+// price for one?
+
+  const Restaurant({
+    required this.id,
+    required this.name,
+    required this.rating,
+    required this.images,
+    required this.subtitle,
+    required this.isPureVeg,
+    required this.costForOneText,
+    required this.deliveryTime,
+    required this.discount,
+    required this.proDiscount,
+  });
+
+  static const all = [
+    Restaurant(
+      id: '1',
+      name: "Domino's Pizza",
+      rating: 4.2,
+      isPureVeg: false,
+      subtitle: 'Pizza, Fast Food, Desserts',
+      costForOneText: '₹350 for one',
+      deliveryTime: '30 mins',
+      discount: '50% OFF',
+      proDiscount: '10% OFF',
+      images: [
+        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        'https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=728&q=80',
+        'https://images.unsplash.com/photo-1571066811602-716837d681de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=536&q=80',
+      ],
+    ),
+    Restaurant(
+      id: '2',
+      name: "The Cake Cafe",
+      rating: 3.9,
+      isPureVeg: true,
+      subtitle: 'Cakes, Bakery',
+      costForOneText: '₹150 for one',
+      deliveryTime: '45 mins',
+      discount: '15% OFF',
+      proDiscount: '8% OFF',
+      images: [
+        'https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
+        'https://images.unsplash.com/photo-1562440499-64c9a111f713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+        'https://images.unsplash.com/photo-1621303837174-89787a7d4729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
+      ],
+    ),
+    Restaurant(
+      id: '3',
+      name: "Burgerama",
+      rating: 4.6,
+      subtitle: 'Burger, Fast Food, Beverages',
+      costForOneText: '₹200 for one',
+      deliveryTime: '20 mins',
+      isPureVeg: false,
+      discount: '20% OFF',
+      proDiscount: '12% OFF',
+      images: [
+        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80',
+        'https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+        'https://images.unsplash.com/photo-1603064752734-4c48eff53d05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+        'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
+      ],
+    ),
+  ];
+
+  static const recommended = all;
+}
---
+++ lib/widgets/restaurant_card.dart
@@ -0,0 +1,309 @@
+import 'package:easy_container/easy_container.dart';
+import 'package:flutter/material.dart';
+import 'package:milkman_interview/models/restaurant.dart';
+import 'package:milkman_interview/widgets/my_cached_network_image.dart';
+import 'package:milkman_interview/widgets/my_image_slider.dart';
+
+class RestaurantCard extends StatelessWidget {
+  static const id = 'RestaurantCard';
+
+  final Restaurant restaurant;
+
+  const RestaurantCard(
+    this.restaurant, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return EasyContainer(
+      color: Colors.white,
+      borderRadius: 10,
+      elevation: 15,
+      padding: 0,
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MyImageSlider(
+                autoPlay: true,
+                showDots: false,
+                photoUrls: restaurant.images,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            restaurant.name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        EasyContainer(
+                          margin: 0,
+                          customPadding: const EdgeInsets.all(5),
+                          elevation: 0,
+                          color: Colors.green[800],
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                restaurant.rating.toStringAsFixed(1),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              const Icon(
+                                Icons.star,
+                                size: 13,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          restaurant.subtitle,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          restaurant.costForOneText,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.access_time,
+                          size: 14,
+                          color: Colors.green[800],
+                        ),
+                        const SizedBox(width: 2.5),
+                        Text(
+                          restaurant.deliveryTime,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const Text(' • '),
+                        Text(
+                          restaurant.discount,
+                          style: const TextStyle(
+                            color: Colors.indigo,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const Text(' • '),
+                        Text(
+                          'Pro ${restaurant.proDiscount}',
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          if (restaurant.isPureVeg)
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Material(
+                color: Colors.green[800]!.withOpacity(0.9),
+                child: const Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text(
+                    'PURE VEG RESTAURANT',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class RestaurantCardVertical extends StatelessWidget {
+  static const id = 'RestaurantCard';
+
+  final Restaurant restaurant;
+
+  const RestaurantCardVertical(
+    this.restaurant, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return EasyContainer(
+      color: Colors.white,
+      borderRadius: 10,
+      height: 270,
+      customMargin: const EdgeInsets.only(bottom: 15),
+      width: 175,
+      elevation: 15,
+      padding: 0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          MyCachedNetworkImage(
+            zoomToFit: true,
+            width: double.infinity,
+            height: 150,
+            url: restaurant.images[0],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        restaurant.name,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    EasyContainer(
+                      margin: 0,
+                      padding: 3,
+                      elevation: 0,
+                      color: Colors.green[800],
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            restaurant.rating.toStringAsFixed(1),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.star,
+                            size: 11,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.access_time,
+                      size: 14,
+                      color: Colors.green[800],
+                    ),
+                    const SizedBox(width: 2.5),
+                    Text(
+                      restaurant.deliveryTime,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.money,
+                      size: 14,
+                      color: Colors.pink,
+                    ),
+                    const SizedBox(width: 2.5),
+                    Text(
+                      restaurant.costForOneText,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 4),
+          SizedBox(
+            width: double.infinity,
+            child: Material(
+              color: Colors.indigo.withOpacity(0.25),
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.ac_unit,
+                      size: 15,
+                      color: Colors.indigo,
+                    ),
+                    const SizedBox(width: 2.5),
+                    Text(
+                      restaurant.discount,
+                      style: const TextStyle(
+                        color: Colors.indigo,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
